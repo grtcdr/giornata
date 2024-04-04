e9dd724d8016d29f311f0d5a036045589791ec90
grtcdr <ba.tahaaziz@gmail.com>
2024-04-04 22:23:40 +0100

    Write tests for giornata-scaffold and giornata-default-format

--- giornata-tests.el
+++ giornata-tests.el
@@ -2,7 +2,7 @@
 (require 'giornata)
 (require 'giornata-calendar)
 
-(defmacro with-deterministic-giornata (&rest body)
+(defmacro with-temporary-giornata-directory (&rest body)
   "Evaluate BODY deterministically.
 
 BODY will be evaluated with `giornata-directory' bound to a
@@ -21,11 +21,36 @@ temporary directory.  `current-time' will be set to a fake time."
 
 (ert-deftest giornata-today ()
   "Check that `giornata-today' works as expected."
-  (with-deterministic-giornata
+  (with-temporary-giornata-directory
    (call-interactively #'giornata-today)
    (call-interactively #'save-buffer)
    (let* ((entries (giornata--entries))
 	  (first (giornata-relative-entry (car entries))))
-     (should (= (length entries) 1))
-     (should (string= first "2024/01/01")))))
+     (should (equal (length entries) 1))
+     (should (string-equal first "2024/01/01")))))
+
+(ert-deftest giornata-scaffold ()
+  "Check that `giornata-scaffold' works as expected."
+  (with-temporary-giornata-directory
+   (let ((giornata-dir-locals
+	  '((fundamental-mode . ((mode . text))))))
+     (call-interactively #'giornata-scaffold)
+     (should
+      (string-equal
+       (format "%S\n" giornata-dir-locals)
+       (with-temp-buffer
+	 (insert-file-contents
+	  (file-name-concat giornata-directory dir-locals-file))
+	 (buffer-string)))))))
+
+(ert-deftest giornata-default-format ()
+  "Check that `giornata-default-format' works as expected."
+  (with-temporary-giornata-directory
+   ;; Without a dir-locals.el, `text' is the default format.
+   (should (equal (giornata-default-format) 'text))
+   ;; Check again with a modified `giornata-dir-locals'
+   (let ((giornata-dir-locals
+	  '((fundamental-mode . ((mode . markdown))))))
+     (call-interactively #'giornata-scaffold))
+   (should (equal (giornata-default-format) 'markdown))))
 
