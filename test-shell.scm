f9aca94fcf85b303a77b63cc31b21c6f23745597
grtcdr <ba.tahaaziz@gmail.com>
2024-04-04 22:00:09 +0100

    Remove git from test-shell.scm manifest

--- test-shell.scm
+++ test-shell.scm
@@ -2,4 +2,4 @@
 ;; for running the integration test suite.
 
 (specifications->manifest
- (list "libfaketime" "emacs-no-x" "coreutils" "make" "git"))
+ (list "libfaketime" "emacs-no-x" "coreutils" "make"))
